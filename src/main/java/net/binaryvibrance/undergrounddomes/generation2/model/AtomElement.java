826cfa599735aa663812d1c782da934aa47b5c03
Steven Blom <atomicblom@binaryvibrance.net>
2014-06-28 00:17:09 +0930

    Mysterious deletes are mysterious

--- src/main/java/net/binaryvibrance/undergrounddomes/generation2/model/AtomElement.java
+++
@@ -1,5 +0,0 @@
-package net.binaryvibrance.undergrounddomes.generation2.model;
-
-public enum AtomElement {
-	Wall, Interior, Untouched, CorridorFloor, Floor, CorridorMidpoint, CorridorEntrance, Debug
-}
